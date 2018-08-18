a3fa66771c6aec4c7d86f43323a5f53b1680401e
Alex Bleasdale <ableasdale@marklogic.com>
2018-08-18 22:45:57 +0100

    Working Admin GUI cluster setup

--- src/main/java/CreateClusterWithAdminGUI.java
+++ src/main/java/CreateClusterWithAdminGUI.java
@@ -25,10 +25,9 @@ public class CreateClusterWithAdminGUI {
         credentialsProvider.addCredentials(Configuration.getInstance().getString("mluser"), Configuration.getInstance().getString("mlpass"));
 
         // 1. Initialize every host in the cluster
-        for (int i=0; i<hosts.length; i++) {
+        for (int i = 0; i < hosts.length; i++) {
             String hostUrl = String.format("http://%s:8001", hosts[i]);
             hostUrls[i] = hostUrl;
-            LOG.info("Initialising: "+hosts[i] + " | "+hostUrls[i]);
             checkPageAndSubmitForm(webClient, hostUrls[i], "Server Install - MarkLogic Server - localhost", "initialize");
         }
         sleep(5000);
@@ -42,8 +41,8 @@ public class CreateClusterWithAdminGUI {
             page = (HtmlPage) input.click();
 
             // 2b: set up admin user
-            if(page.getTitleText().contains("Security Setup - MarkLogic Server")){
-                LOG.info("Confirmed "+ hosts[0] + " is now at the Security Setup Form");
+            if (page.getTitleText().contains("Security Setup - MarkLogic Server")) {
+                LOG.info(String.format("Confirmed %s is now at the Security Setup Form", hosts[0]));
             }
 
             HtmlForm securityForm = page.getFormByName("security");
@@ -55,60 +54,41 @@ public class CreateClusterWithAdminGUI {
             input = securityForm.getInputByName("ok");
             page = (HtmlPage) input.click();
 
-            if(page.getTitleText().contains("System Summary - MarkLogic Server")){
-                LOG.info("Confirmed "+ hostUrls[0] + " is the configured bootstrap host");
+            if (page.getTitleText().contains("System Summary - MarkLogic Server")) {
+                LOG.info(String.format("Confirmed %s is the configured bootstrap host", hostUrls[0]));
             }
             LOG.info("------------------------------------------------------------------");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //System.exit(0);
 
         // 3. Add remaining hosts to cluster
         for (int i = 1; i < hosts.length; i++) {
-            LOG.info("Adding "+hosts[i]+" to cluster with " + hosts[0] + " as the bootstrap host");
+            LOG.info(String.format("Adding %s to cluster with %s as the bootstrap host", hosts[i], hosts[0]));
             try {
-                LOG.info("1. Introduce host "+hosts[i]+" to the cluster (join-admin)");
+                LOG.info(String.format("Introduce host %s to the cluster (join-admin)", hosts[i]));
                 HtmlPage page = webClient.getPage(hostUrls[i]);
-                if(page.getTitleText().contains("Join a Cluster - MarkLogic Server")){
-                    LOG.info("Confirmed "+ hosts[i] + " is ready to start the process of joining the cluster");
+                if (page.getTitleText().contains("Join a Cluster - MarkLogic Server")) {
+                    LOG.info(String.format("Confirmed %s is ready to start the process of joining the cluster", hosts[i]));
                 }
                 HtmlForm form = page.getFormByName("join-admin");
                 form.getInputByName("server").setValueAttribute(hosts[0]);
                 HtmlImageInput input = form.getInputByName("ok");
                 page = (HtmlPage) input.click();
                 String redirectUrl = getRedirectUrl(page);
-
-                LOG.info("2. Handling redirect: "+redirectUrl);
-                page = checkPageAndSubmitForm(webClient, hostUrls[0] + redirectUrl, "Join a Cluster - MarkLogic Server - "+hosts[0], "accept-joiner");
-                //sleep(20000);
-                //LOG.info(page.asXml());
-                LOG.info("3. Accept joiner");
-                page = clickOk(page,"accept-joiner-confirm");
-                LOG.info("4. Accept joiner");
-                page = clickOk(page,"joined-admin");
-                LOG.info("5. Joiner added? - sleep and check");
-                //redirectUrl = getRedirectUrl(page);
-                //page = webClient.getPage(hostUrls[0]+redirectUrl);
+                page = checkPageAndSubmitForm(webClient, String.format("%s%s", hostUrls[0], redirectUrl), String.format("Join a Cluster - MarkLogic Server - %s", hosts[0]), "accept-joiner");
+                page = clickOk(page, "accept-joiner-confirm");
+                page = clickOk(page, "joined-admin");
                 sleep(10000);
                 page = webClient.getPage(hostUrls[i]);
-                // LOG.info(checkTheSecondHost.asXml());
-                if(page.getTitleText().equals("Cluster Summary - MarkLogic Server - "+hosts[i])){
-                    LOG.info(hosts[i]+" has been successfully added to the cluster");
+                if (page.getTitleText().equals(String.format("Cluster Summary - MarkLogic Server - %s", hosts[i]))) {
+                    LOG.info(String.format("%s has been successfully added to the cluster", hosts[i]));
                 }
-
-                //LOG.info(page.asXml());
-                // The host should now be connected and will be restarting.
-                // LOG.info(page.getTitleText());
-                //page = webClient.getPage(hostUrls[i]);
-                //LOG.info(page.getTitleText());
-
+                LOG.info("------------------------------------------------------------------");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
 
     private static String getRedirectUrl(HtmlPage page) {
@@ -124,15 +104,11 @@ public class CreateClusterWithAdminGUI {
 
     private static HtmlPage clickOk(HtmlPage page, String formName) {
         try {
-            //LOG.info("Form name: "+formName + " | "+page.getTitleText());
             HtmlForm form = page.getFormByName(formName);
-            //LOG.info("Form action: "+form.getActionAttribute());
             HtmlImageInput okBtn = form.getInputByName("ok");
-           // HtmlPage newPage = (HtmlPage) okBtn.click();
-            //LOG.info(newPage.asXml());
-            return (HtmlPage) okBtn.click(); //return (HtmlPage) okBtn.click();
+            return (HtmlPage) okBtn.click();
         } catch (IOException e) {
-            LOG.error("IO Exception: ",e);
+            LOG.error("IO Exception: ", e);
         }
         return null;
     }
@@ -140,18 +116,14 @@ public class CreateClusterWithAdminGUI {
     private static HtmlPage checkPageAndSubmitForm(WebClient webClient, String hostUri, String titleElement, String formName) {
         try {
             HtmlPage page = webClient.getPage(hostUri);
-            LOG.info("page.getTitleText():"+page.getTitleText());
-            // LOG.info(page.asXml());
-            if(page.getTitleText().equals(titleElement)){
-                LOG.info(String.format("%s confirmed page title as: %s", hostUri, titleElement));
-            } else {
-                LOG.info(page.getTitleText());
+            if (page.getTitleText().equals(titleElement)) {
+                LOG.info(String.format("Confirmed page title as: %s", titleElement));
             }
             HtmlForm form = page.getFormByName(formName);
             HtmlImageInput okBtn = form.getInputByName("ok");
             return (HtmlPage) okBtn.click();
         } catch (IOException e) {
-           LOG.error("IO Exception: ",e);
+            LOG.error("IO Exception: ", e);
         }
         return null;
     }
